21e642c9cf88bf796b0ad33cbcdf23a574484957
Adrián Serrano <aserranod@minsait.com>
2020-04-22 17:11:40 +0200

    Added timestamp to recipe schema

--- src/recipes/schemas/recipe.schema.ts
+++ src/recipes/schemas/recipe.schema.ts
@@ -1,20 +1,23 @@
 import { Schema } from 'mongoose'
 
-export const RecipeSchema: Schema = new Schema({
-  title: { type: String, required: true, unique: true },
-  picture: { type: String, required: true },
-  description: { type: String, required: true },
-  dificulty: { type: Number, required: true },
-  portions: { type: Number, requiered: true },
-  time: { type: Number, required: true },
-  ingredients: [
-    {
-      food: {
-        type: Schema.Types.ObjectId,
-        ref: 'Food'
-      },
-      quantity: Number
-    }
-  ],
-  steps: [{ number: Number, description: String }]
-})
+export const RecipeSchema: Schema = new Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    picture: { type: String, required: true },
+    description: { type: String, required: true },
+    dificulty: { type: Number, required: true },
+    portions: { type: Number, requiered: true },
+    time: { type: Number, required: true },
+    ingredients: [
+      {
+        food: {
+          type: Schema.Types.ObjectId,
+          ref: 'Food'
+        },
+        quantity: Number
+      }
+    ],
+    steps: [{ number: Number, description: String }]
+  },
+  { timestamps: true }
+)
