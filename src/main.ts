2862a6f3ea80530758506c665f4e0889071dabdf
Adrián Serrano <aserranod@minsait.com>
2020-04-25 20:54:08 +0200

    Added HTTPS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import * as fs from 'fs'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/server.key'),
+    cert: fs.readFileSync('./secrets/server.crt')
+  }
+  const app = await NestFactory.create(AppModule, { httpsOptions })
   const port = process.env.API_PORT
   await app.listen(port)
   console.log(`Application is running on port: ${port}`)
